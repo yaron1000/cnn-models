76c48d50b844b7eb2cd4839b09a618f4a3293a56
Iker <iker.sanchez@vizzuality.com>
2018-12-05 09:51:15 +0100

    changed workflow

---
+++ preprocess/ee_collection_specifics.py
@@ -0,0 +1,197 @@
+"""
+Information on Earth Engine collections stored here (e.g. bands, collection ids, etc.)
+"""
+
+import ee
+import numpy as np
+
+def ee_bands(collection):
+    """
+    Earth Engine band names
+    """
+    
+    dic = {
+        'Sentinel2': ['B4','B3','B2','B8'],
+        'Landsat7': ['B3','B2','B1','B4'],
+        'CroplandDataLayers': ['cropland']
+    }
+    
+    return dic[collection]
+
+def ee_bands_rgb(collection):
+    """
+    Earth Engine band names
+    """
+    
+    dic = {
+        'Sentinel2': ['B4','B3','B2'],
+        'Landsat7': ['B3','B2','B1'],
+        'CroplandDataLayers': ['cropland']
+    }
+    
+    return dic[collection]
+
+def ee_collections(collection):
+    """
+    Earth Engine image collection names
+    """
+    dic = {
+        'Sentinel2': 'COPERNICUS/S2',
+        'Landsat7': 'LANDSAT/LE07/C01/T1_SR',
+        'CroplandDataLayers': 'USDA/NASS/CDL'
+    }
+    
+    return dic[collection]
+
+def normDiff_bands(collection):
+    """
+    Earth Engine normDiff bands
+    """
+    dic = {
+        'Sentinel2': [['B8','B4'], ['B8','B3']],
+        'Landsat7': [['B4','B3'], ['B4','B2']],
+        'CroplandDataLayers': []
+    }
+    
+    return dic[collection]
+
+def normDiff_band_names(collection):
+    """
+    Earth Engine normDiff bands
+    """
+    dic = {
+        'Sentinel2': ['ndvi', 'ndwi'],
+        'Landsat7': ['ndvi', 'ndwi'],
+        'CroplandDataLayers': []
+    }
+    
+    return dic[collection]
+
+## ------------------------- Filter datasets ------------------------- ##
+## Lansat 7 Cloud Free Composite
+def CloudMaskL7(image):
+    qa = image.select('pixel_qa')
+    #If the cloud bit (5) is set and the cloud confidence (7) is high
+    #or the cloud shadow bit is set (3), then it's a bad pixel.
+    cloud = qa.bitwiseAnd(1 << 5).And(qa.bitwiseAnd(1 << 7)).Or(qa.bitwiseAnd(1 << 3))
+    #Remove edge pixels that don't occur in all bands
+    mask2 = image.mask().reduce(ee.Reducer.min())
+    return image.updateMask(cloud.Not()).updateMask(mask2)
+
+def CloudFreeCompositeL7(Collection_id, startDate, stopDate, geom):
+    ## Define your collection
+    collection = ee.ImageCollection(Collection_id)
+
+    ## Filter 
+    collection = collection.filterBounds(geom).filterDate(startDate,stopDate)\
+            .map(CloudMaskL7)
+
+    ## Composite
+    composite = collection.median()
+    
+    return composite
+
+## Sentinel 5 Cloud Free Composite
+def CloudMaskS2(image):
+    """
+    European Space Agency (ESA) clouds from 'QA60', i.e. Quality Assessment band at 60m
+    parsed by Nick Clinton
+    """
+    qa = image.select('QA60')
+
+    # Bits 10 and 11 are clouds and cirrus, respectively.
+    cloudBitMask = int(2**10)
+    cirrusBitMask = int(2**11)
+
+    # Both flags set to zero indicates clear conditions.
+    mask = qa.bitwiseAnd(cloudBitMask).eq(0).And(\
+            qa.bitwiseAnd(cirrusBitMask).eq(0))
+
+    return image.updateMask(mask).divide(10000)
+
+def CloudFreeCompositeS2(Collection_id, startDate, stopDate, geom):
+    ## Define your collection
+    collection = ee.ImageCollection(Collection_id)
+
+    ## Filter 
+    collection = collection.filterBounds(geom).filterDate(startDate,stopDate)\
+            .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))\
+            .map(CloudMaskS2)
+
+    ## Composite
+    composite = collection.median()
+    
+    return composite
+
+## Cropland Data Layers
+def CroplandData(Collection_id, startDate, stopDate, geom):
+    ## Define your collection
+    collection = ee.ImageCollection(Collection_id)
+
+    ## Filter 
+    collection = collection.filterBounds(geom).filterDate(startDate,stopDate)
+
+    ## First image
+    image = ee.Image(collection.first())
+    
+    return image
+
+## ------------------------------------------------------------------- ##
+
+def Composite(collection):
+    dic = {
+        'Sentinel2': CloudFreeCompositeS2,
+        'Landsat7': CloudFreeCompositeL7,
+        'CroplandDataLayers': CroplandData
+    }
+    
+    return dic[collection]
+
+## ------------------------ Change data types ------------------------ ##
+
+def DataTypeS2(image):
+
+    ## Change RGB data type to unsigned int8.
+    RGB = image.expression('(400 * RGB)', {'RGB': image.select(['B4', 'B3', 'B2'])})
+
+    RGB = RGB.byte()
+
+    ## Change NIR data type to unsigned int16.
+    NIR = image.expression('(400 * NIR)', {'NIR': image.select(['B8'])})
+
+    NIR = NIR.byte().rename('B8')
+
+    ## Change NDVI and NDWI data types to unsigned int8.
+    NDVI = image.expression('(300 * NDVI)', {'NDVI': image.select(['ndvi'])});
+      
+    NDVI = NDVI.byte().rename('ndvi')
+    
+    NDWI = image.expression('(300 * NDWI)', {'NDWI': image.select(['ndwi'])});
+
+    NDWI = NDWI.byte().rename('ndwi')
+    
+    ## Concatenate Bands with new data types
+    image = ee.Image.cat([RGB, NIR, NDVI, NDWI])
+
+    return image
+
+
+## ------------------------------------------------------------------- ##
+
+def Dtype(collection):
+    dic = {
+        'Sentinel2': DataTypeS2,
+        'Landsat7': [],
+        'CroplandDataLayers': []
+    }
+    
+    return dic[collection]
+
+def h5py_dtype(collection):
+    dic = {
+        'Sentinel2': np.uint8,
+        'Landsat7': np.uint8,
+        'CroplandDataLayers': np.uint8
+    }
+    
+    return dic[collection]
---
+++ train.py
@@ -0,0 +1,218 @@
+from keras.models import model_from_json
+from keras.optimizers import Adam 
+from keras.callbacks import Callback, ModelCheckpoint
+import h5py
+import json
+import argparse
+import numpy as np
+from preprocess.preprocess_datasets import categorical_data
+from models import SegNet
+
+class LossHistory(Callback):
+    def __init__(self, root_out, losses):
+        self.root_out = root_out        
+        self.losses = losses
+
+    def on_epoch_end(self, batch, logs={}):
+        self.losses.append(logs)
+        with open("{0}_loss.json".format(self.root_out), 'w') as f:
+            json.dump(self.losses, f)
+
+    def finalize(self):
+        pass
+        
+class train(object):
+    
+    def __init__(self, root_in, root_out, nClasses, model_name, option):
+        """
+        Class used to train models
+        Parameters
+        ----------
+        root_in : string
+            Path of the input files.
+        root_out : string
+            Path of the output files.
+        nClasses : int
+            Number of classes.
+        model_name : string
+            Model name.
+        option : string
+            Indicates what needs to be done (start or continue).
+        """
+        
+        self.root_in = root_in
+        self.root_out = root_out
+        self.nClasses = nClasses
+        self.option = option
+        self.model_name = model_name
+        
+        self.batch_size = 32
+        
+        self.models = {'segnet': SegNet.segnet}
+        
+        self.input_x_train = self.root_in + "x_train.hdf5"
+        self.input_y_train = self.root_in + "y_train.hdf5"
+
+        self.input_x_validation = self.root_in + "x_validation.hdf5"
+        self.input_y_validation = self.root_in + "y_validation.hdf5"
+        
+        tmp = np.load(self.root_in + 'normalization_values.npz')
+        self.min_v, self.max_v = tmp['arr_0'], tmp['arr_1']
+
+        f = h5py.File(self.input_x_train, 'r')
+        self.n_train_orig, self.ny, self.nx, self.nBands = f.get("x_train").shape        
+        f.close()
+
+        f = h5py.File(self.input_y_validation, 'r')
+        self.n_validation_orig, _, _, _ = f.get("y_validation").shape        
+        f.close()
+        
+        self.batchs_per_epoch_training = int(self.n_train_orig / self.batch_size)
+        self.batchs_per_epoch_validation = int(self.n_validation_orig / self.batch_size)
+
+        self.n_training = self.batchs_per_epoch_training * self.batch_size
+        self.n_validation = self.batchs_per_epoch_validation * self.batch_size
+
+        print("Original training set size: {0}".format(self.n_train_orig))
+        print("   - Final training set size: {0}".format(self.n_training))
+        print("   - Batch size: {0}".format(self.batch_size))
+        print("   - Batches per epoch: {0}".format(self.batchs_per_epoch_training))
+
+        print("Original validation set size: {0}".format(self.n_validation_orig))
+        print("   - Final validation set size: {0}".format(self.n_validation))
+        print("   - Batch size: {0}".format(self.batch_size))
+        print("   - Batches per epoch: {0}".format(self.batchs_per_epoch_validation))
+        
+        print("Number of Bands: {0}".format(self.nBands))
+        print("Number of Classes: {0}".format(self.nClasses))
+        
+    def training_generator(self):
+        f_x = h5py.File(self.input_x_train, 'r')
+        x = f_x.get("x_train")
+
+        f_y = h5py.File(self.input_y_train, 'r')
+        y = f_y.get("y_train")
+
+        while True:        
+            for i in range(self.batchs_per_epoch_training):
+
+                input_train = x[i*self.batch_size:(i+1)*self.batch_size,:,:,:].astype('float32')            
+                output_train = y[i*self.batch_size:(i+1)*self.batch_size,:,:,:].astype('uint8')
+                
+                # Normalize input
+                for n in range(len(self.min_v)):
+                    input_train[:,:,:,n] = (input_train[:,:,:,n]-self.min_v[n])/(self.max_v[n]-self.min_v[n])
+                
+                # Change class labels
+                output_train = categorical_data(output_train)
+                
+                yield input_train, output_train
+
+        f_x.close()
+        f_y.close()
+
+    def validation_generator(self):
+        f_x = h5py.File(self.input_x_train, 'r')
+        x = f_x.get("x_validation")
+
+        f_y = h5py.File(self.input_y_train, 'r')
+        y = f_y.get("y_validation")
+        
+        while True:        
+            for i in range(self.batchs_per_epoch_validation):
+                
+                input_validation = x[i*self.batch_size:(i+1)*self.batch_size,:,:,:].astype('float32')            
+                output_validation = y[i*self.batch_size:(i+1)*self.batch_size,:,:,:].astype('uint8')
+                
+                # Normalize input
+                for n in range(len(self.min_v)):
+                    input_validation[:,:,:,n] = (input_validation[:,:,:,n]-self.min_v)/(self.max_v-self.min_v)
+                    
+                # Change class labels
+                output_validation = categorical_data(output_validation)
+
+                yield input_validation, output_validation
+
+        f_x.close()
+        f_y.close()
+
+    def define_network(self):
+        print("Setting up network...")
+    
+        model = self.models[self.model_name]
+
+        self.model = model((self.ny, self.nx, self.nBands), self.nClasses)
+        
+        # Save model
+        json_string = self.model.to_json()
+        f = open('{0}_model.json'.format(self.root_out+self.model_name), 'w')
+        f.write(json_string)
+        f.close()
+
+    def read_network(self):
+        print("Reading previous network...")
+                
+        f = open('{0}_model.json'.format(self.root_out), 'r')
+        json_string = f.read()
+        f.close()
+
+        self.model = model_from_json(json_string)
+        self.model.load_weights("{0}_weights.hdf5".format(self.root_out+self.model_name))
+        
+    def compile_network(self):        
+        self.model.compile(loss='mse', optimizer=Adam(lr=1e-4))
+
+    def train_network(self, nEpochs):
+        print("Training network...")        
+        
+        # Recover losses from previous run or set and empty one
+        if (self.option == 'continue'):
+            with open("{0}_loss.json".format(self.root_out+self.model_name), 'r') as f:
+                losses = json.load(f)
+        else:
+            losses = []
+  
+        # To saves the model weights after each epoch if the validation loss decreased
+        self.checkpointer = ModelCheckpoint(filepath="{0}_weights.hdf5".format(self.root_out+self.model_name), verbose=1, save_best_only=True)
+        # To save a list of losses over each batch 
+        self.history = LossHistory(self.root_out, losses) # saving a list of losses over each batch 
+    
+        # Train the network
+        self.metrics = self.model.fit_generator(self.training_generator(), steps_per_epoch=self.n_training, epochs=nEpochs, 
+            callbacks=[self.checkpointer, self.history], validation_data=self.validation_generator(), validation_steps=self.n_validation)
+        
+        self.history.finalize()
+
+        
+
+if (__name__ == '__main__'):
+
+    parser = argparse.ArgumentParser(description='Train SegNet')
+    parser.add_argument('-i','--in', help='Input files path')
+    parser.add_argument('-o','--out', help='Output files path')
+    parser.add_argument('-c','--classes', help='Number of classes', default=4)
+    parser.add_argument('-e','--epochs', help='Number of epochs', default=10)
+    parser.add_argument('-m','--model_name', help='Output files path', default = "")
+    parser.add_argument('-a','--action', help='Action', choices=['start', 'continue'], required=True)
+    parsed = vars(parser.parse_args())
+
+    root_in = str(parsed['in'])
+    root_out = str(parsed['out'])
+    nClasses = int(parsed['classes'])
+    nEpochs = int(parsed['epochs'])
+    model_name = str(parsed['model_name'])
+    option = parsed['action']
+
+    out = train(root_in, root_out, nClasses, model_name, option)
+
+    if (option == 'start'):           
+        out.define_network()        
+        
+    if (option == 'continue'):
+        out.read_network()
+
+    if (option == 'start' or option == 'continue'):
+        out.compile_network()
+        out.train_network(nEpochs)
+
+
